09a4181ec97bf9ebe4dc18e74193ef876568a3a1
Tomas Zaoral <zaoralt@gmail.com>
2016-06-06 03:03:28 +0100

    before updating mutation probs

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/core.async "0.2.374"]])
+                 [org.clojure/math.numeric-tower "0.0.4"]])
--- src/clojea/adaptive.clj
+++ src/clojea/adaptive.clj
@@ -1,31 +1,36 @@
 (ns clojea.adaptive
   (:use [clojea.random.mtrandom]))
 
-
 (defn rand-gene [max]
   (inc (tlmt-rand max)))
 
-(defn rand-chromo [alleles]
+(defn rand-chrom [alleles]
   (mapv rand-gene alleles))
 
 (defn rand-chroms [n alleles]
-  (repeatedly n (partial rand-chromo alleles)))
+  (repeatedly n (partial rand-chrom alleles)))
 
 (defn eval-chroms [ff chroms]
   (->> chroms
-       (map #(hash-map :chrom % :fitness (ff %)))
-       (sort-by :fitness >)))
+       (map #(hash-map :chrom % :fness (ff %)))
+       (sort-by :fness >)))
 
-(defn linear-probs [n]
-  (let [decn (take n (iterate dec n))
-        sum (double (* n (/ (+ n 1) 2)))]
-    (map #(/ % sum) decn)))
+(defn normalize [coll]
+  (let [sum (double (apply + coll))]
+    (map #(/ % sum) coll)))
 
 (defn cumul-probs [n]
-  (let [incn (take n (iterate inc 1))
-        cumuln (map #(/ 1 %) incn)
-        sum (double (apply + cumuln))]
-    (map #(/ % sum) cumuln)))
+  (map #(/ 1 %) (take n (iterate inc 1))))
+
+(defn unite [pop-size & pops]
+  (loop [ps (vec pops) r []]
+    (if (> pop-size (count r))
+      (let [[m i] (->> (map #(vector (first %1) %2) ps (range))
+                       (filter (comp some? first))
+                       (reduce (fn [[m _ :as max] [x _ :as c]]
+                                 (if (> (:fness x) (:fness m)) c max))))]
+        (recur (update ps i rest) (conj r m)))
+      r)))
 
 (defn select [coll probs]
   (let [coll-probs (map vector coll probs)
@@ -37,12 +42,6 @@
       0
       coll-probs)))
 
-(defn select-two [coll probs]
-  (let [x (select coll probs)]
-    [x (->> (repeatedly (partial select coll probs))
-            (filter (partial not= x))
-            (first))]))
-
 (defn rand-pivot [n]
   (inc (tlmt-rand (dec n))))
 
@@ -51,39 +50,37 @@
    (vec (concat (take pivot y) (drop pivot x)))])
 
 (defn mutate [chrom alleles mutation-p]
-  (mapv
-    (fn [g a] (if (tlmt-rand-boolean mutation-p) (rand-gene a) g))
-    chrom alleles))
+  (mapv (fn [g a] (if (tlmt-rand-boolean mutation-p)
+                    (->> (repeatedly #(rand-gene a)) (filter #(not= g %)) (first))
+                    g))
+        chrom alleles))
 
 (defn pnl [msg]
   (fn [x] (println msg x) x))
 
 (defn evolve []
   (let [pop-size 20
-        elitism 2
-        crossover-p 1
         mutation-p 0.01
-        selection-p (cumul-probs pop-size)
-        alleles (repeat 100 2)
+        selection-p (normalize (cumul-probs pop-size))
+        alleles (interleave (repeat 50 100) (repeat 50 10))
         ff (partial apply +)
         max-iter 1000
-        accept-sol (ff alleles)
+        accept-sol (* 0.95 (ff alleles))
         eval (partial eval-chroms ff)]
     (loop [pop (eval (rand-chroms pop-size alleles)) i 0]
-      (if (and (not= max-iter i) (> accept-sol (:fitness (first pop))))
+      (if (and (> max-iter i) (> accept-sol (:fness (first pop))))
         (recur
-          (eval
-            (concat
-              (->> (take elitism pop)
-                   (map :chrom))
-              (->> (repeatedly (partial select-two pop selection-p))
-                   (map #(map :chrom %))
-                   (mapcat (fn [[x y :as xy]] (if (tlmt-rand-boolean crossover-p)
-                                                (crossover x y (rand-pivot (count alleles))) xy)))
-                   (map #(mutate % alleles mutation-p))
-                   (take (- pop-size elitism)))))
+          (unite pop-size
+                 pop
+                 (eval
+                   (->> (repeatedly (partial select pop selection-p))
+                        (map :chrom)
+                        (partition 2)
+                        (mapcat #(crossover (first %) (second %) (rand-pivot (count alleles))))
+                        (map #(mutate % alleles mutation-p))
+                        (take pop-size))))
           (inc i))
-        [i (:fitness (first pop))]))))
+        [i (:fness (first pop))]))))
 
 (def experiments 100)
 
@@ -95,9 +92,4 @@
                   (mapv #(+ %1 %2) a x))
                 [0 0] (repeatedly experiments f))))
 
-(comment
-  (measure evolve)                                          ;; no elitism [1000.0 9657.06]
-  (measure evolve)                                          ;; w/ elitism [1000.0 9774.24]
-  (measure evolve)                                          ;; w/ 1.0 COP [1000.0 9787.65]
-  (measure evolve)                                          ;; [112.1 200.0]
-  )
+;; [215.64 5227.52]
